45d601e1625e80555108b21f793ebb81c4efe4eb
kavyapandey <pandeykavya2@gmail.com>
2022-04-19 23:31:29 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { useState } from 'react';
-import { Route, Router, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Home';
-import Login from './Login';
+import { useState } from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Login from "./Login";
 
 function App() {
-  const [isLoggedIn,setUserLoggedIn] = useState(false);
+  const [isLoggedIn, setUserLoggedIn] = useState(false);
 
-  const loginStatus = (status) =>{
-    setUserLoggedIn(status)
-  }
-  return (
-    <>
-    {isLoggedIn ? (<Home/>) : (<Login loginStatus={loginStatus}/>)}
-    </>
-  );
+  const loginStatus = (status) => {
+    setUserLoggedIn(status);
+  };
+  return <>{isLoggedIn ? <Home /> : <Login loginStatus={loginStatus} />}</>;
 }
 
 export default App;
